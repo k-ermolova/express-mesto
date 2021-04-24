36e74eaf1ce5895901ed137ea451fdb290f1abe2
mysoulterious <ermolowaksenia@yandex.ru>
2021-04-24 17:10:12 +0300

    feat: connect with server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT);
