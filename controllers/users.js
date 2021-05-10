052d6b93e2f471bcc66541609472df2b639862e3
mysoulterious <ermolowaksenia@yandex.ru>
2021-05-10 15:40:08 +0300

    feat: add email and password

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -33,9 +34,14 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.postUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, about, avatar })
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
